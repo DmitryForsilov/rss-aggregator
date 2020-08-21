393b9e28238b3b174d5238596af0d32db728542a
DmitryForsilov <tpadtpad@yandex.ru>
2020-08-21 12:04:08 +0300

    refactoring

--- src/parse.js
+++ src/parse.js
@@ -1,8 +1,14 @@
-const parse = (data) => {
+const parse = (rawData) => {
   const xmlParser = new DOMParser();
-  const parsedData = xmlParser.parseFromString(data, 'text/xml');
+  const xml = xmlParser.parseFromString(rawData, 'text/xml');
+  const title = xml.querySelector('channel > title').textContent;
+  const postsData = [...xml.querySelectorAll('item')]
+    .map((postNode) => ({
+      title: postNode.querySelector('title').textContent,
+      link: postNode.querySelector('link').textContent,
+    }));
 
-  return parsedData;
+  return { title, postsData };
 };
 
 export default parse;
